b04782385b12aaf9f12fa31bd3627fe84ac129cc
komarnytskiyivan <komar.ivan.97@gmail.com>
2020-11-29 00:42:30 +0200

    Beautified code

--- includes/buying.php
+++ includes/buying.php
@@ -1,19 +1,19 @@
 <?php
 include "./config.php";
-    mysqli_query($connection, "INSERT INTO `pays`  (`prev_balance`, `sum`, `next_balance`) VALUES (" . $_POST['prev_balance'] . ", " . $_POST['sum'] . ", " . $_POST['next_balance'].")") or die(mysqli_error($connection));
-   $pays = mysqli_query($connection,"SELECT * FROM `pays` ORDER BY `id` DESC LIMIT 1");
-   while($pay = mysqli_fetch_assoc($pays)){
-   ?>
-<tr>
-   <td class="text-center" style="width: 20%;">
-        <p><?php echo $pay['prev_balance']; ?> </p>
-   </td>
-   <td class="text-center" style="width: 20%;">
-        <p><?php echo $pay['sum']; ?> </p>
-   </td>
-   <td class="text-center" style="width: 20%;">
-        <p><?php echo $pay['next_balance']; ?> </p>
-   </td>
-</tr>
-<?php
-   }
+mysqli_query($connection, "INSERT INTO `pays`  (`prev_balance`, `sum`, `next_balance`) VALUES (" . $_POST['prev_balance'] . ", " . $_POST['sum'] . ", " . $_POST['next_balance'].")") or die(mysqli_error($connection));
+$pays = mysqli_query($connection,"SELECT * FROM `pays` ORDER BY `id` DESC LIMIT 1");
+while($pay = mysqli_fetch_assoc($pays)){
+     ?>
+     <tr>
+     <td class="text-center" style="width: 20%;">
+          <p><?php echo $pay['prev_balance']; ?> </p>
+     </td>
+     <td class="text-center" style="width: 20%;">
+          <p><?php echo $pay['sum']; ?> </p>
+     </td>
+     <td class="text-center" style="width: 20%;">
+          <p><?php echo $pay['next_balance']; ?> </p>
+     </td>
+     </tr>
+     <?php
+}
---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'testingivan',
+        'password' => 'Ytytuh1000',
+        'name' => 'testingivan'
+    )
+);
+require "db.php";
+?>
